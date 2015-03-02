54bbc42a552d9edb7be7cdb6b8a3fd9ab10b2980
Chris Rahnejat <chris.rahnejat@capco.com>
2015-03-02 18:58:20 +0000

    requirements added and admin screen works but is a bit slow because of fk onto a table of 1000+ objects (answers) - not a bug, just how it is

--- yoccore/admin.py
+++ yoccore/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
+# from django.contrib import admin
+from django.forms import TextInput, Textarea
+from nested_inline.admin import *
+from yoccore import models as m
 
-# Register your models here.
+class DefaultAdmin(NestedModelAdmin):
+    list_display = ('id', '__unicode__')
+    actions_on_top = True
+    save_as = True
+    save_on_top = True
+    list_per_page = 30
+    ordering = ['id']
+    formfield_overrides = {
+    models.CharField: {'widget': Textarea(attrs={'rows': 8, 'cols': 60})},
+    models.TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 60})},
+    models.DecimalField: {'widget': TextInput(attrs={'size': '10'})},
+    }
+    list_display_links = ('id', '__unicode__')
+
+class DefaultViewer(NestedTabularInline):
+    save_as = True
+    save_on_top = True
+    extra = 3
+    ordering = ['id']
+    formfield_overrides = {
+    models.CharField: {'widget': Textarea(attrs={'rows': 4, 'cols': 60})},
+    models.TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 60})},
+    models.DecimalField: {'widget': TextInput(attrs={'size': '10'})},
+    }
+
+admin.site.register(m.Session, DefaultAdmin)
+admin.site.register(m.Question, DefaultAdmin)
+admin.site.register(m.Answer, DefaultAdmin)
+admin.site.register(m.CleanedAnswer, DefaultAdmin)
